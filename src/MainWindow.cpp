ed469e9384d70726126667adabbb88fc942a9cb7
Krzysztof Bąk <github@itachi.pl>
2024-07-16 22:22:44 +0200

    [GH-14] Reformat MainWindow.cpp.

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -6,98 +6,101 @@
  */
 #include "MainWindow.h"
 
-#include "resource.h"
-
 #include <commctrl.h>
 
-static const wchar_t CLASS_NAME[] = APP_TITLE;
+#include "resource.h"
+
+static constexpr wchar_t CLASS_NAME[] = APP_TITLE;
 
 MainWindow *MainWindow::instance = nullptr;
 
 MainWindow::MainWindow(const HINSTANCE hInstance)
-        : windowInstance(hInstance) {
-    INITCOMMONCONTROLSEX icex = {};
-    icex.dwICC = ICC_LISTVIEW_CLASSES;
-    InitCommonControlsEx(&icex);
+    : windowInstance(hInstance), windowHandle(), hotkeyTable() {
+  INITCOMMONCONTROLSEX icex = {};
+  icex.dwICC = ICC_LISTVIEW_CLASSES;
+  InitCommonControlsEx(&icex);
 
-    WNDCLASSW wc = {};
-    wc.lpfnWndProc = windowProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = CLASS_NAME;
+  WNDCLASSW wc = {};
+  wc.lpfnWndProc = windowProc;
+  wc.hInstance = hInstance;
+  wc.lpszClassName = CLASS_NAME;
 
-    RegisterClassW(&wc);
+  RegisterClassW(&wc);
 
-    mainIcon = LoadIconW(hInstance, MAKEINTRESOURCE(ID_ICON_MAIN));
+  mainIcon = LoadIconW(hInstance, MAKEINTRESOURCE(ID_ICON_MAIN));
 }
 
-LRESULT MainWindow::windowProc(HWND hwnd, UINT uMsg, WPARAM wParam,
-                               LPARAM lParam) {
-    MainWindow *self = MainWindow::instance;
+LRESULT MainWindow::windowProc(const HWND hwnd, const UINT uMsg,
+                               const WPARAM wParam, const LPARAM lParam) {
+  MainWindow *self = instance;
 
-    switch (uMsg) {
-        case WM_CREATE:
-            self->hotkeyTable.addToWindow(hwnd, self->windowInstance);
-            return 0;
-        case WM_NOTIFY:
-            self->hotkeyTable.handleWmNotify(lParam);
-            return 0;
-        case WM_NULL: {
-            PROCESS_PATH_BUFF processPathBuffer;
-            KEYSTROKE_BUFF keystrokeBuffer;
-            DWORD proc_id;
+  switch (uMsg) {
+    case WM_CREATE:
+      self->hotkeyTable.addToWindow(hwnd, self->windowInstance);
+      return 0;
+    case WM_NOTIFY:
+      self->hotkeyTable.handleWmNotify(lParam);
+      return 0;
+    case WM_NULL: {
+      PROCESS_PATH_BUFF processPathBuffer;
+      KEYSTROKE_BUFF keystrokeBuffer;
+      DWORD proc_id;
 
-            GetWindowThreadProcessId((HWND)wParam, &proc_id);
+      GetWindowThreadProcessId(reinterpret_cast<HWND>(wParam), &proc_id);
 
-            Core::keystrokeToString(lParam, keystrokeBuffer);
-            Core::getProcessPath(proc_id, processPathBuffer);
+      Core::keystrokeToString(lParam, keystrokeBuffer);
+      Core::getProcessPath(proc_id, processPathBuffer);
 
-            self->hotkeyTable.addEntry(keystrokeBuffer, processPathBuffer);
+      self->hotkeyTable.addEntry(keystrokeBuffer, processPathBuffer);
 
-            return 0;
-        }
-        case WM_DESTROY:
-            delete self;
-
-            PostQuitMessage(0);
-            return 0;
+      return 0;
     }
+    case WM_DESTROY:
+      delete self;
 
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+      PostQuitMessage(0);
+      return 0;
+    default:
+      break;
+  }
+
+  return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
 MainWindow *MainWindow::GetInstance(HINSTANCE hInstance) {
-    if (MainWindow::instance == nullptr) {
-        MainWindow::instance = new MainWindow(hInstance);
-        MainWindow::instance->createWindow();
-    }
+  if (instance == nullptr) {
+    instance = new MainWindow(hInstance);
+    instance->createWindow();
+  }
 
-    return MainWindow::instance;
+  return instance;
 }
 
 void MainWindow::createWindow() {
-    windowHandle = CreateWindowExW(0,
-                                   CLASS_NAME,
-                                   APP_TITLE,
-                                   WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU,
-                                   CW_USEDEFAULT,
-                                   CW_USEDEFAULT,
-                                   600,
-                                   300,
-                                   NULL,
-                                   NULL,
-                                   windowInstance,
-                                   NULL);
+  windowHandle = CreateWindowExW(0,
+                                 CLASS_NAME,
+                                 APP_TITLE,
+                                 WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU,
+                                 CW_USEDEFAULT,
+                                 CW_USEDEFAULT,
+                                 600,
+                                 300,
+                                 nullptr,
+                                 nullptr,
+                                 windowInstance,
+                                 nullptr);
 
-    if (mainIcon != NULL) {
-        SendMessage(windowHandle, WM_SETICON, ICON_BIG, (LPARAM)mainIcon);
-    }
+  if (mainIcon != nullptr) {
+    SendMessage(
+        windowHandle, WM_SETICON, ICON_BIG, reinterpret_cast<LPARAM>(mainIcon));
+  }
 
-    core.setMainWindowThreadId(windowHandle);
-    core.setHooks();
+  core.setMainWindowThreadId(windowHandle);
+  core.setHooks();
 }
 
 MainWindow::~MainWindow() {
-    if (mainIcon != NULL) {
-        DestroyIcon(mainIcon);
-    }
+  if (mainIcon != nullptr) {
+    DestroyIcon(mainIcon);
+  }
 }
